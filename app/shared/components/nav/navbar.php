c3c6ae023fb577199d6aa21b2e07fa1987dfcc93
RDPW1SH <rafael10ambrosio@gmail.com>
2024-12-17 14:12:01 +0000

    teste local

--- app/shared/components/nav/navbar.php
+++ app/shared/components/nav/navbar.php
@@ -1,14 +1,19 @@
 <?php
 
-if(isset($_SESSION['auth'])) {
+    
+?>
 
-    echo ("<nav>
+<nav>
         <div class='nav_container'>
             <h1>😺</h1>
             <form action='/pesquisar/pesquisar.php' method='GET'>
                 <div style='display: flex; gap: 10px; align-items: center;'>
                     <input type='text' name='pesquisa' placeholder='pesquise os gatos'>
-                    <h2>Bem vindo</h2>
+                    <?php if(isset($_SESSION['auth'])) {
+                        echo ("<a href='./user/profile.php'>Bem Vindo</button>");
+                    } else {
+                        echo("<a href='./login.php'><button type='submit' name='login_btn'>Login</button></a>");
+                    }?>    
                 </div>
             </form>
         </div>
@@ -31,37 +36,3 @@ if(isset($_SESSION['auth'])) {
             }
         </style>
     </nav>
-");
-    
-} else {
-    echo ("<nav>
-        <div class='nav_container'>
-            <h1>😺</h1>
-            <form action='/pesquisar/pesquisar.php' method='GET'>
-                <div style='display: flex; gap: 10px; align-items: center;'>
-                    <input type='text' name='pesquisa' placeholder='pesquise os gatos'>
-                    <a href='./login.php'><button type='submit' name='login_btn'>Login</button></a>
-                </div>
-            </form>
-        </div>
-        <style>
-            nav {
-                padding: 0;
-                background-color: violet;
-            }
-            .nav_container {
-                display: flex;
-                padding: 0px 20px;
-                gap: 10px;
-                justify-content: space-between;
-                align-items: center;
-            }
-            input {
-                padding: 6px 3px;
-                width: 150px;
-                height: 25px;
-            }
-        </style>
-    </nav>");
-}
-?>
